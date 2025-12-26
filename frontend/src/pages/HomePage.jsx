a41d0ee89d81fedad06f666d7b5621641631ab8a
unknown <arpitdnegi@gmail.com>
2025-12-26 20:22:27 +0530

    Fix notes rendering conditions in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -46,9 +46,9 @@ const HomePage = () => {
     <div className='max-w-7xl mx-auto p-4 mt-6'>
          {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-         {Array.isArray(notes) && notes.length && !isRateLimited && <NotesNotFound/>}
+         {Array.isArray(notes) && notes.length ===0 && !loading && !isRateLimited && <NotesNotFound/>}
 
-    {Array.isArray(notes) && notes.length === 0 && !isRateLimited && (
+    {Array.isArray(notes) && notes.length > 0 && !isRateLimited && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
               <NoteCard key={note._id} note={note} setNotes={setNotes} />
